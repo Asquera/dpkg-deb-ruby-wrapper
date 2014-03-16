f644584500d6d969ba7e0e4519e59c4e2e5259bb
Felix Gilcher <felix.gilcher@asquera.de>
2014-03-16 15:08:40 +0000

    read fields from a deb's control file
    wrap the dpgk-deb -f command, use a simplistic approach to parsing and return
    the fields requested. Not a beauty in itself, but it works.

--- lib/dpkg/deb.rb
+++ lib/dpkg/deb.rb
@@ -3,5 +3,41 @@ require "dpkg/deb/version"
 module Dpkg
   module Deb
     # Your code goes here...
+    class << self
+
+      def fields(package_path, fieldnames = [])
+        out = `dpkg-deb -f #{package_path} #{fieldnames.join(" ")}`
+
+        # rules are:
+        # fields are usually line-based and follow the general format of
+        # <field name><colon><space><field value>, for example
+        # Maintainer: Felix Gilcher <felix.gilcher@asquera.de>
+        # The exception to the rule is the decription field that has the format
+        # <field name><colon><space><short description><newline>
+        # <description>
+        # The description needs to be indented by a single space and empty lines
+        # are replaced by a dot. 
+
+        fields = {}
+        description = []
+
+        out.each_line do |line|
+
+          if match = /^(?<fieldname>[^:]+): (?<fieldvalue>.+)$/.match(line)
+            fields[match['fieldname']] = match['fieldvalue']
+          elsif match = /^ .+$/.match(line)
+            description.push line
+          else
+            raise "unparseable line '#{line}' found"
+          end
+
+        end
+
+        fields['Description'] << description.join("") unless fields['Description'].nil?
+
+        fields 
+      end
+
+    end
   end
 end
